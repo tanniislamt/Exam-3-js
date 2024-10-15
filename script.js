247c4eaff7f42de470cb9ab7143e6921eaeeead5
Your Name <you@example.com>
2024-10-15 16:56:01 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+function myFunction(){
+    document.getElementById("name").innerHTML = "MIM";
+}
+
+
+function change(){
+    document.getElementById('demo').innerHTML = "Tonni";
+}
+
+
+
+function myimage() {
+    document.getElementById('myimage').src='image/WhatsApp Image 2024-08-24 at 14.44.40.jpeg'
+   }
+ 
+ function image(){
+     document.getElementById('myimage').src='image/Tanni.jpeg'
+ }
+
+
+
+
+function fontChange(){
+    document.getElementById('font').style.fontSize="35px";
+}
+
+
+function friend(){
+    document.getElementById('friend-none').style.display="none"
+}
+
+
+function showtext(){ document.getElementById('show').style.display='block'
+}
